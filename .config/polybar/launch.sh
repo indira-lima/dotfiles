9836b59258a24d9911a8059fa62e345417e4f9e0
Dahan Schuster <dan.plschuster@gmail.com>
2022-10-27 00:03:30 -0300

    Polybar: First configuration for multiple screens

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,15 @@
 polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar-home.log
-polybar home 2>&1 | tee -a /tmp/polybar-home.log & disown
+#echo "---" | tee -a /tmp/polybar-home.log
+#polybar home 2>&1 | tee -a /tmp/polybar-home.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload home &
+  done
+else
+  polybar --reload home &
+fi
 
 echo "Polybar launched..."
