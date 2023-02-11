88d3bd9eb9d74a09d3a77c3771a3806d38571287
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-11 19:28:21 -0300

    picom: use a new picom fork with better animations
    After asking Reddit what could be wrong with my previous Picom
    (animations weren't triggered when switching workspaces), someone
    answered that this is a good fork
    
    It is indeed

--- picom/launch.sh
+++ picom/launch.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
 
-killall arian8j2-picom
-arian8j2-picom --config ~/.config/picom/picom.conf &
+killall ornfelt-picom
+ornfelt-picom \
+	--animations \
+	--animation-for-open-window=fly-in \
+	--experimental-backends \
+	--config ~/.config/picom/picom.conf \
+	-b
+
+# killall arian8j2-picom
+# arian8j2-picom \
+# 	--config ~/.config/picom/picom.conf \
+# 	-b
