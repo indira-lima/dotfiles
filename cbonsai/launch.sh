acef80f9821b1f94ab6f0b302a6a799b2bec4da0
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-13 12:17:05 -0300

    cbonsai: use a fixed seed
    Fixes generating different trees with the same progress file, as the
    default seed uses `date +%x`, which changes every second

--- cbonsai/launch.sh
+++ cbonsai/launch.sh
@@ -7,5 +7,14 @@
 CBONSAI_DIR="$HOME/.config/cbonsai"
 PROGRESS_FILE="$CBONSAI_DIR/progress"
 
-cbonsai --save $PROGRESS_FILE --load $PROGRESS_FILE --live --infinite --multiplier=15 --wait=2 --time=1 --life=100 --base=1 --leaf="&,@,#,e,n,b,y"
+cbonsai \
+	--save $PROGRESS_FILE \
+	--load $PROGRESS_FILE \
+	--live --infinite \
+	--multiplier=15 \
+	--wait=120 --time=1 \
+	--life=100 \
+	--base=1 \
+	--leaf="&,@,#,e,n,b,y" \
+	--seed 42
 
