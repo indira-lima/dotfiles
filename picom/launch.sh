19f2e2d32439bf8f525397ce2ae009972fa02bd5
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-13 00:39:11 -0300

    picom: going back to arian8j2's picom
    Didn't like ornfelt's fork as much as arian8j2's,
    so I'm using my simple but clean transition script

--- picom/launch.sh
+++ picom/launch.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
-killall ornfelt-picom
-ornfelt-picom \
-	--animations \
-	--animation-for-open-window=fly-in \
-	--experimental-backends \
-	--config ~/.config/picom/picom.conf \
-	-b
-
-# killall arian8j2-picom
-# arian8j2-picom \
+# killall ornfelt-picom
+# ornfelt-picom \
+# 	--animations \
+# 	--animation-for-open-window=fly-in \
+# 	--animation-for-open-window=fly-in \
+# 	--experimental-backends \
 # 	--config ~/.config/picom/picom.conf \
 # 	-b
+
+killall arian8j2-picom
+arian8j2-picom \
+	--config ~/.config/picom/picom.conf \
+	-b
