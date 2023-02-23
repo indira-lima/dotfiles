87ff66b47bb9332ebc0e0c7219d4df857ab57a62
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-22 21:11:13 -0300

    i3wm: change index of bonsai and keepass workspaces

--- cbonsai/open_or_launch.sh
+++ cbonsai/open_or_launch.sh
@@ -7,7 +7,7 @@
 isBonsaiOpen=$(i3-msg -t get_tree | jq -r | grep '"class": "Bonsai"')
 CBONSAI_DIR="$HOME/.config/cbonsai"
 
-i3-msg workspace 9
+i3-msg workspace 10
 
 if [ ! -n "$isBonsaiOpen" ]; then
 	kitty --class Bonsai --title Bonsai --config "$CBONSAI_DIR/kitty.conf" sh -c "$CBONSAI_DIR/launch.sh" 
