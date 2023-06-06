906c7f424f30aa39cb993a2cbe197becafb03d72
Dahan Schuster <dan.plschuster@gmail.com>
2023-06-05 21:29:15 -0300

    polybar: terminate previous process using killall

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,7 +2,7 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
+killall polybar
 
 # Launch bar1 and bar2
 #echo "---" | tee -a /tmp/polybar-home.log
